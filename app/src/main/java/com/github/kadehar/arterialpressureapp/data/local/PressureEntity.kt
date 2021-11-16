5b31aee6cc1ab0b2f47bd019e7ed9501f39968dc
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2021-11-16 09:33:20 +0300

    Initial commit

---
+++ app/src/main/java/com/github/kadehar/arterialpressureapp/data/local/PressureEntity.kt
@@ -0,0 +1,18 @@
+package com.github.kadehar.arterialpressureapp.data.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.github.kadehar.arterialpressureapp.data.local.PressureEntity.Companion.TABLE_NAME
+
+@Entity(tableName = TABLE_NAME)
+data class PressureEntity(
+    @PrimaryKey @ColumnInfo(name = "id") val id: String,
+    @ColumnInfo(name = "morning") val morning: String,
+    @ColumnInfo(name = "evening") val evening: String,
+    @ColumnInfo(name = "timestamp") val timestamp: Long
+) {
+    companion object {
+        const val TABLE_NAME = "pressure"
+    }
+}
